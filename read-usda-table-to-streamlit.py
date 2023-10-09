544e311400890259628654fa611547821cf15aa9
cnagbim <cnagbim@gmail.com>
2023-10-09 14:03:41 -0600

    added script for streamlit app

---
+++ read-usda-table-to-streamlit.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 13:45:11 2023
+
+@author: Nelo_Agbim
+"""
+
+import pandas as pd
+import requests
+import io
+import streamlit as st
+
+# Downloading the csv file from your GitHub account
+
+url = "https://raw.githubusercontent.com/neloagbim/usda-data-summary/main/usda-datasets.csv" # Make sure the url is the raw version of the file on GitHub
+
+download = requests.get(url).content
+
+# Reading the downloaded content and turning it into a pandas dataframe
+
+df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+
+# Printing out the first 5 rows of the dataframe
+
+print (df.head())
+
+# create streamlit header
+
+st.header("USDA Datasets for Research Projects")
