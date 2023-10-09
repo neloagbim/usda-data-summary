8e9294e96c04ea78ee55d44ec65e4a2d95a726ff
Nelo Agbim <47784696+neloagbim@users.noreply.github.com>
2023-10-09 14:30:06 -0600

    Update read-usda-table-to-streamlit.py
    added text input search box

--- read-usda-table-to-streamlit.py
+++ read-usda-table-to-streamlit.py
@@ -14,19 +14,22 @@ import streamlit as st
 
 url = "https://raw.githubusercontent.com/neloagbim/usda-data-summary/main/usda-datasets.csv" # Make sure the url is the raw version of the file on GitHub
 
-#download = requests.get(url).content
-
+# use following for loading github csv when running locally
+# download = requests.get(url).content
 # Reading the downloaded content and turning it into a pandas dataframe
+# df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 
-#df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+# read the url of the github csv
 df = pd.read_csv(url)
 
-# Printing out the first 5 rows of the dataframe
-
-print (df.head())
+# Printing out the first 5 rows of the dataframe to test results
+#print (df.head())
 
 # create streamlit header
-
 st.header("USDA Datasets for Research Projects")
 
+# add text input for searching description column
+st.text_input("Search dataset description."," ")
+
+# show dataframae as table
 st.dataframe(df)
